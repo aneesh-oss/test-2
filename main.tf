bd73b6d54f99059a42d5e74dcfc5b1f9799d8965
Aneesh-oss <creadora08@gmail.com>
2024-05-16 16:44:22 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    newrelic = {
+      source = "newrelic/newrelic"
+      version = "3.36.0"
+    }
+  }
+}
+
+provider "newrelic" {
+  # Configuration options
+  account_id = 4438265
+  api_key = "NRAK-WKLYV84JTU0CUS2XXRXJJRZZ7VN"
+  region = "US"
+}
+
+module "policy" {
+    source = "./modules/policy"
+}
+module "dashboard" {
+    source = "./modules/dashboard"
+}
